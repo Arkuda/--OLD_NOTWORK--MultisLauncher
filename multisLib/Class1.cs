ebf7af31204d4f73d2a31fcd1efb46831d655dfc
Arkuda <reyzor2142@gmail.com>
2013-01-05 17:28:51 +0400

    Download Commite
    Download Commite

---
+++ multisLib/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace multisLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ multisLib/ModPack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace multisLib
+{
+    class ModPack
+    {
+        private string name, author, version, url, dir, mcVersion, serverUrl, logoName, imageName, info, animation, xml, isOp;
+        private string[] mods;
+        private string image, logo;
+
+    }
+}
