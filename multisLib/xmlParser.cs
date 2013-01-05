b63193d7850b93f46df408da2c454ab64d1f059b
Arkuda <reyzor2142@gmail.com>
2013-01-05 22:03:19 +0400

    Add calss xmlParser
    Add calss xmlParser

---
+++ multisLib/xmlParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace multisLib
+{
+    class xmlParser
+    {
+
+        public ModPack parseModpack(string xml)
+        {
+            
+            
+
+        }
+
+
+    }
+}
