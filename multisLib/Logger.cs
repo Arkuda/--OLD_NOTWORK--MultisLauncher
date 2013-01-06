37850f2904873926da710910e2dcad5e52a1a972
Arkuda <reyzor2142@gmail.com>
2013-01-06 13:58:30 +0400

    Add Logger

---
+++ multisLib/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace multisLib
+{
+    class Logger
+    {
+
+
+
+
+        public void printMsg(string message)
+        {
+            
+        }
+        public void printErr(string error)
+        {
+            
+        }
+
+        public void printText(string text)
+        {
+            
+        }
+
+        public void printCustom(string index, string customText)
+        {
+            
+        }
+    }
+}
