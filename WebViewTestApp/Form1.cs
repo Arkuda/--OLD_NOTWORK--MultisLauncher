d4598f415dd16644d9a674c1d3d90cd98aaa86c6
Arkuda <reyzor2142@gmail.com>
2013-01-08 11:40:01 +0400

    Add
    Added:
    Project - test webViev Gui writer

---
+++ WebViewTestApp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebViewTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.Document.GetElementById("logo").Click += Form1_Click;
+        }
+
+        void Form1_Click(object sender, HtmlElementEventArgs e)
+        {
+
+            MessageBox.Show("HEHEY HEY");
+            throw new NotImplementedException();
+        }
+
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+           // MessageBox.Show(e.Url.ToString()); 
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+              //MessageBox.Show(e.Url.ToString());  
+        }
+
+
+
+        
+           
+        
+
+    
+    }
+}
