5a642f0680a76cd0348b070e29c3e3b81f109ba7
Arkuda <reyzor2142@gmail.com>
2013-01-08 15:01:21 +0400

    Addet WebView Page (BoilerPlate custom)
    Addet WebView Page (BoilerPlate custom)

---
+++ multisLauncher/WebViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace multisLauncher
+{
+    class WebViewController
+    {
+        /*
+         * TO LOAD LOCAL HTML PAGE
+         *  StorageFile     sf = await Package.Current.InstalledLocation.GetFileAsync("TestPage.html");
+         *  uint            length = (uint)sf.Size;
+         *  IInputStream    input = await sf.OpenForReadAsync();
+         *  byte[]          buffer = new byte[length];
+         *  await input.ReadAsync(buffer.AsBuffer(), length, InputStreamOptions.None);
+         *  string          content = Encoding.GetEncoding("utf-8").GetString(buffer, 0, buffer.Length);
+         *  webView.NavigateToString(content);
+        */
+        
+    }
+}
